65d5901482d3074d10a4711018f359af26370b4f
chakir elarabi <116119429+codewithchakir@users.noreply.github.com>
2023-05-06 18:36:25 +0100

    added type to user and removed the breeze interface from the register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,58 @@
-<x-guest-layout>
+
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
         <!-- Name -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+            <label for="name">name</label>
+            <input id="name" type="text" name="name" required autofocus autocomplete="name" />
         </div>
 
         <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <div>
+            <label for="email">Email</label>
+            <input id="email" type="email" name="email" required autocomplete="username" />
+        </div>
+
+        <!-- Type  -->
+        <div>
+            <label for="type">type</label>
+            <select id="type" name="type" required>
+                <option value="" selected disabled hidden>'Choose an option'</option>
+            
+                <option value="student">student</option>
+                <option value="prof">prof</option>
+                <option value="supervisor">supervisor</option>
+            </select>
         </div>
 
         <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+        <div>
+            <label for="password">Password</label>
 
-            <x-text-input id="password" class="block mt-1 w-full"
+            <input id="password"
                             type="password"
                             name="password"
                             required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+        <div>
+            <label for="password_confirmation">Confirm Password</label>
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
+            <input id="password_confirmation"
                             type="password"
                             name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
+        <div>
+            <a href="{{ route('login') }}">
+                Already registered?
             </a>
 
-            <x-primary-button class="ml-4">
-                {{ __('Register') }}
-            </x-primary-button>
+            <button>
+                Register
+            </button>
         </div>
     </form>
-</x-guest-layout>
+
